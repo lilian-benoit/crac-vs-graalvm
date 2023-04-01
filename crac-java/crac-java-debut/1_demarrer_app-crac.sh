c4287d88da674af7f69dd443682b8a4bee67efec
Lilian BENOIT <lilian.benoit@lbenoit.fr>
2023-04-01 22:48:32 +0200

    Ajout script sh pour le lancement

---
+++ crac-java/crac-java-debut/1_demarrer_app-crac.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# https://www.coolgenerator.com/ascii-text-generator
+# Big
+echo "   _____ _____        _____                     _____  _____  "
+echo "  / ____|  __ \      / ____|              /\   |  __ \|  __ \ "
+echo " | |    | |__) |__ _| |       ______     /  \  | |__) | |__) |"
+echo " | |    |  _  // _\` | |      |______|   / /\ \ |  ___/|  ___/ "
+echo " | |____| | \ \ (_| | |____            / ____ \| |    | |     "
+echo "  \_____|_|  \_\__,_|\_____|          /_/    \_\_|    |_|     "
+echo "                                                              "
+                                                              
+if [ "$JAVA_HOME" == "" ]; then
+  echo "Vous devez définir la variable JAVA_HOME"
+  exit -1;
+fi
+
+ARCHIVE=`ls target/*.jar | cut -d'/' -f2`
+
+echo "$JAVA_HOME/bin/java -XX:CRaCCheckpointTo=/tmp/crac-files/java -jar target/$ARCHIVE"
+$JAVA_HOME/bin/java -XX:CRaCCheckpointTo=/tmp/crac-files/java -jar target/$ARCHIVE
---
+++ crac-java/crac-java-debut/3_restaurer-crac.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "   _____ _____        _____            _____  ______  _____ _______      _    _ _____  ______ "
+echo "  / ____|  __ \      / ____|          |  __ \|  ____|/ ____|__   __|/\  | |  | |  __ \|  ____|"
+echo " | |    | |__) |__ _| |       ______  | |__) | |__  | (___    | |  /  \ | |  | | |__) | |__   "
+echo " | |    |  _  // _\` | |      |______| |  _  /|  __|  \___ \   | | / /\ \| |  | |  _  /|  __|  "
+echo " | |____| | \ \ (_| | |____           | | \ \| |____ ____) |  | |/ ____ \ |__| | | \ \| |____ "
+echo "  \_____|_|  \_\__,_|\_____|          |_|  \_\______|_____/   |_/_/    \_\____/|_|  \_\______|"
+echo "                                                                                              "
+                                                                                              
+if [ "$JAVA_HOME" == "" ]; then
+  echo "Vous devez définir la variable JAVA_HOME"
+  exit -1;
+fi
+
+ARCHIVE=`ls target/*.jar | cut -d'/' -f2`
+
+echo "$JAVA_HOME/bin/java -XX:CRaCRestoreFrom=/tmp/crac-files/java"
+$JAVA_HOME/bin/java -XX:CRaCRestoreFrom=/tmp/crac-files/java
